a0fa7d6a89619bda19ff9a19f18a96308334db45
elruffo <leonardo.ruffin@gmail.com>
2015-06-18 12:12:46 +0200

    Change spacing in frontoffice.urls

--- frontoffice/urls.py
+++ frontoffice/urls.py
@@ -3,11 +3,8 @@ from django.conf.urls import patterns
 
 urlpatterns = patterns(
     'frontoffice.views',
-
     # Home
     (r'^[/]?$', 'home', {'template_name': 'frontoffice/home.html'}, 'frontoffice_home'),
-
     # API
     (r'^search[/]?$', 'search', {}, 'search'),
-
 )
